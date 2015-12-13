892fbba23105886cc8c6ecf67b53ce71bab8b61e
Jordan <jordan@emporioht.com.br>
2015-12-13 15:18:59 -0200

    Banco
    SQL para gerar o Banco

---
+++ app.sql
@@ -0,0 +1,24 @@
+﻿# Host: localhost  (Version: 5.6.17)
+# Date: 2015-12-13 15:11:02
+# Generator: MySQL-Front 5.3  (Build 4.263)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "usuario"
+#
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(255) NOT NULL DEFAULT '',
+  `login` varchar(100) NOT NULL DEFAULT '',
+  `senha` varchar(32) NOT NULL DEFAULT '',
+  `ativo` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "usuario"
+#
+
