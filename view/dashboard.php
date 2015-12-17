e338703b29648e42b38d1d8d6ef38208c218ef8c
Jordan <jordan@emporioht.com.br>
2015-12-17 20:58:49 -0200

    Adicionando icones
    Usando icones de fontes

--- view/dashboard.php
+++ view/dashboard.php
@@ -7,75 +7,64 @@
 	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 	<?php 
 		$this
-			->getAsset('app.css');
-			
+			->getAsset('font-awesome.min.css')
+			->getAsset('app.css')
+			->getAsset('estilo.css');
 		?>
 </head>
 <body>
 	<div class="container app-main">
 		
-		<div class="row text-center">
-			<div class="janela janela-board">
-				<header>Titulo</header>
+		<a href="#menu" class="trigger-menu">
+			<i class="fa fa-bars"></i>
+		</a>
+		<div class="card card-menu off-canvas" id="canvasMenu">
+			<header class="titulo">
+				<a href="#menu">
+					<i class="fa fa-bars"></i>
+				</a>
+				Menu
+			</header>
+			<ul>
+				<li><a href="#">MENU 1</a></li>
+				<li><a href="#">MENU 2</a></li>
+				<li><a href="#">MENU 3</a></li>
+				<li><a href="#">MENU 4</a></li>
+				<li><a href="#">MENU 5</a></li>
+				<li><a href="#">MENU 6</a></li>
+				<li><a href="#">MENU 7</a></li>
+				<li><a href="#">MENU 8</a></li>
+				<li><a href="#">MENU 9</a></li>
+				<li><a href="#">MENU 10</a></li>
+			</ul>
+		</div>
+		
+		<div class="card card-user " data-drop="true">
+			<a href="#usuario" title="Fulano de Tal">
+				<img src="http://placehold.it/40x40?text=User" alt="Usuário">
+			</a>
+			<div class="card card-opcoes">
 				<div class="corpo">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sapiente fugiat, provident quos amet, explicabo optio modi accusamus. Consectetur sequi suscipit magni adipisci voluptates expedita deleniti officia delectus velit at!
-				</div>
-			</div>
-			<div class="janela janela-board">
-				<header class="text-center">Titulo</header>
-				<div class="corpo">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sapiente fugiat, provident quos amet, explicabo optio modi accusamus. Consectetur sequi suscipit magni adipisci voluptates expedita deleniti officia delectus velit at!
-				</div>
-			</div>
-
-			<div class="janela janela-board">
-				<header class="text-right">Titulo</header>
-				<div class="corpo">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sapiente fugiat, provident quos amet, explicabo optio modi accusamus. Consectetur sequi suscipit magni adipisci voluptates expedita deleniti officia delectus velit at!
+					<a href="#User">Fulano</a>
+					<hr>
+					<a href="#sair">Sair</a>
 				</div>
 			</div>
 		</div>
-		<div class="row text-center">
-			
-			<div class="janela janela-board">
-				<div class="corpo">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sapiente fugiat, provident quos amet, explicabo optio modi accusamus. Consectetur sequi suscipit magni adipisci voluptates expedita deleniti officia delectus velit at!
-				</div>
-				<div class="rodape">
-					Titulo Rodape
-				</div>
-			</div>
 
-			<div class="janela janela-board">
-				<div class="corpo">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sapiente fugiat, provident quos amet, explicabo optio modi accusamus. Consectetur sequi suscipit magni adipisci voluptates expedita deleniti officia delectus velit at!
-				</div>
-				<div class="rodape text-center">
-					Titulo Rodape
-				</div>
-			</div>
-
-			<div class="janela janela-board">
-				<div class="corpo">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sapiente fugiat, provident quos amet, explicabo optio modi accusamus. Consectetur sequi suscipit magni adipisci voluptates expedita deleniti officia delectus velit at!
-				</div>
-				<div class="rodape text-right">
-					Titulo Rodape
-				</div>
+		<div class="janela janela-dashboard">
+			<header>Paginas</header>
+			<div class="corpo">
+				
 			</div>
 		</div>
-		<div class="row text-center">
-			<div class="janela janela-board">
-				<div class="corpo">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sapiente fugiat, provident quos amet, explicabo optio modi accusamus. Consectetur sequi suscipit magni adipisci voluptates expedita deleniti officia delectus velit at!
-				</div>
-			</div>
-		</div>
+		
 
 	</div>
 	<?php 
 		$this
-			->getAsset('lib/jquery-1.11.3.min.js');
+			->getAsset('lib/jquery-1.11.3.min.js')
+			->getAsset('app.js');
 	?>
 
 </body>
