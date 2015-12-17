83a8da7f0ec04f3fcc26f050782fd7e6c653e628
Jordan <jordan@emporioht.com.br>
2015-12-17 13:08:23 -0200

    Modos de exibicao de janelas
    Opcoes basicas de janelas

--- view/dashboard.php
+++ view/dashboard.php
@@ -14,11 +14,68 @@
 <body>
 	<div class="container app-main">
 		
+		<div class="row text-center">
+			<div class="janela janela-board">
+				<header>Titulo</header>
+				<div class="corpo">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sapiente fugiat, provident quos amet, explicabo optio modi accusamus. Consectetur sequi suscipit magni adipisci voluptates expedita deleniti officia delectus velit at!
+				</div>
+			</div>
+			<div class="janela janela-board">
+				<header class="text-center">Titulo</header>
+				<div class="corpo">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sapiente fugiat, provident quos amet, explicabo optio modi accusamus. Consectetur sequi suscipit magni adipisci voluptates expedita deleniti officia delectus velit at!
+				</div>
+			</div>
+
+			<div class="janela janela-board">
+				<header class="text-right">Titulo</header>
+				<div class="corpo">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sapiente fugiat, provident quos amet, explicabo optio modi accusamus. Consectetur sequi suscipit magni adipisci voluptates expedita deleniti officia delectus velit at!
+				</div>
+			</div>
+		</div>
+		<div class="row text-center">
+			
+			<div class="janela janela-board">
+				<div class="corpo">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sapiente fugiat, provident quos amet, explicabo optio modi accusamus. Consectetur sequi suscipit magni adipisci voluptates expedita deleniti officia delectus velit at!
+				</div>
+				<div class="rodape">
+					Titulo Rodape
+				</div>
+			</div>
+
+			<div class="janela janela-board">
+				<div class="corpo">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sapiente fugiat, provident quos amet, explicabo optio modi accusamus. Consectetur sequi suscipit magni adipisci voluptates expedita deleniti officia delectus velit at!
+				</div>
+				<div class="rodape text-center">
+					Titulo Rodape
+				</div>
+			</div>
+
+			<div class="janela janela-board">
+				<div class="corpo">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sapiente fugiat, provident quos amet, explicabo optio modi accusamus. Consectetur sequi suscipit magni adipisci voluptates expedita deleniti officia delectus velit at!
+				</div>
+				<div class="rodape text-right">
+					Titulo Rodape
+				</div>
+			</div>
+		</div>
+		<div class="row text-center">
+			<div class="janela janela-board">
+				<div class="corpo">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sapiente fugiat, provident quos amet, explicabo optio modi accusamus. Consectetur sequi suscipit magni adipisci voluptates expedita deleniti officia delectus velit at!
+				</div>
+			</div>
+		</div>
+
 	</div>
 	<?php 
 		$this
-			->getAsset('lib/jquery-1.11.3.min.js')
-			->getAsset('login.js');
+			->getAsset('lib/jquery-1.11.3.min.js');
 	?>
 
 </body>
