ae261b18d6e7883e0dcf877f014185d802493a81
Jordan <jordan@emporioht.com.br>
2015-12-19 17:53:08 -0200

    Classe para upload de arquivos deimagens

--- bsync.bat
+++ bsync.bat
@@ -1,4 +1,4 @@
 @echo off
 title APP SYNC
 
-browser-sync start --proxy "localhost/site" --files "assets/**/*, view/**" "c:/wamp/www/site/"
+browser-sync start --proxy "localhost/site" --files "assets/**/*, view/dashboard/**" "c:/wamp/www/site/"
