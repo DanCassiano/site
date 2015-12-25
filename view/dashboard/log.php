9118ba171d1d4611c591c8a0f84ac8c71a6c2bf9
Jordan <jordan@emporioht.com.br>
2015-12-25 16:09:12 -0200

    Preparando a view de Log

---
+++ view/dashboard/log.php
@@ -0,0 +1,34 @@
+<div class="janela janela-dashboard">
+	<header>
+		Log 
+		<div class="controle right">
+			<input type="text" placeholder="Buscar...">
+		</div>
+		<div class="controle right">
+			<select name="" id="filtroLog">
+				<option value="1">Opt 1</option>
+				<option value="2">Opt 2</option>
+				<option value="3">Opt 3</option>
+				<option value="4">Opt 4</option>
+				<option value="5">Opt 5</option>
+			</select>
+		</div>
+		<div class="controle right">
+			<select name="" id="filtroLog2">
+				<option value="1">Opt 10</option>
+				<option value="2">Opt 20</option>
+				<option value="3">Opt 30</option>
+				<option value="4">Opt 40</option>
+				<option value="5">Opt 50</option>
+			</select>
+		</div>
+	</header>
+	<div class="corpo">
+		
+	</div>
+</div>
+
+<?php 
+
+	$this->getAsset("log.js");
+ ?>
