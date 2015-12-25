b4ab498627a4429a5474c1383ae2c4b48dc9f97a
Jordan <jordan@emporioht.com.br>
2015-12-25 16:06:57 -0200

    Plugin de Select js

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,9 +14,9 @@
 			$("[data-drop='true']").DropDown();
 			this.__bindMenu();
 
-			$("[data-dialogo=true]").Dialog({
-				trigger: $("[data-trigger]")
-			})
+			// $("[data-dialogo=true]").Dialog({
+			// 	trigger: $("[data-trigger]")
+			// })
 		}
 
 		app.__bindMenu = function(){
@@ -37,6 +37,70 @@
 			
 			return true;
 		}
+
+	$.fn.Select = function( opcoes ) {
+
+		var padrao = {
+			texto: "Filtro",
+			open: function(r){},
+			close:function(r){},
+			change:function(r){}
+		}
+
+		return $(this).each(function(){
+				
+				var $ele = $(this);
+					$ele.hide();
+
+				var op = $.extend({}, padrao, opcoes );
+				var dados = [],
+					htmlLista = "";
+
+					$.each( $ele.find("option"),function(i,v){
+						dados.push({ "value": $(v).val(), "texto": $(v).text()});
+						htmlLista += "<li class=\"\"><a href=\""+$(v).val()+"\">"+$(v).text()+"</a></li>";
+					});
+
+					if( $ele.attr('id') == undefined)
+						$ele.attr('id', "select"+Math.floor(Math.random() * 100) );
+
+				var select = $("<div class='select'></div>");
+					$ele.before(select);
+
+					select.html('<a href="#select">'+
+									'<span>'+op.texto+'</span>&nbsp;'+
+									'<i class="fa fa-caret-down"></i>'+
+								'</a>'+
+								'<div class="select-lista">'+
+									'<ul>'+	htmlLista+'</ul>'+
+								'</div>');
+
+				var lista = select.find(".select-lista");
+				var width = lista.outerWidth() < 50 ? 50 : lista.outerWidth();
+					select.width( width  );
+
+				select.on("click",'a[href="#select"]',function(e){
+					e.preventDefault();
+					lista.toggle();
+				});
+
+				lista.on('click', 'a',function(e){
+						e.preventDefault();
+						lista.find("li").removeClass('select');
+						$(this).parent().addClass("select");
+						valorSelecionado = $(this).attr('href');
+					
+						select
+							.find(">a span")
+							.text( $(this).text() )
+							.attr('href', $(this).attr('href') );
+						lista.hide();
+					});
+			});
+
+		return $(this);
+	}
+
 
 	$.fn.DropDown = function( opcoes ){
 
