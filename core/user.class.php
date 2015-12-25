37b123c93089ea6b8e655db0d38d2391c4978dc1
Jordan <jordan@emporioht.com.br>
2015-12-25 16:09:01 -0200

    Adicao da chamada da funcao de log

--- core/user.class.php
+++ core/user.class.php
@@ -20,18 +20,32 @@
 			return $this->pdo;
 		}
 
+		public function logoff() {
+			$pdo = $this->getPDO();
+			$pdo->setLog( "Logoff usuario " , Session::get("login") );
+			unset($_SESSION['login']);
+			Session::destroyToken();
+		}
 
 		public function login( $email, $senha ) {
 
-			$senha = md5(md5(md5(md5( $senha ))));
 			$pdo = $this->getPDO();
+			$msg = "Tentativa de Login : e-mail:{$email}  senha:{$senha} ";
+			$id = 0;
+
+			$senha = md5(md5(md5(md5( $senha ))));
 			$r = $pdo->read( "SELECT id FROM usuarios WHERE email = :email AND senha = :senha AND ativo = 1", 
 							 "email={$email}&senha={$senha}" );
+
 
 			if( $pdo->getRowCount() > 0) {
 				$this->logado = $r[0]['id'];
 				Session::set('login', $this->logado );
+				$msg .= " <strong>Sucesso</strong> ";
+				$id =$r[0]['id'];
 			}
+
+			$pdo->setLog( $msg , $id );
 			return $this->logado;
 		}
 
@@ -69,5 +83,28 @@
 			$pdo = $this->getPDO();
 			$dados['senha'] = md5(md5(md5(md5( $dados['senha'] ))));
 			echo $pdo->insert( "usuarios", $dados );
+		}
+
+		public function getPermissoes( $idUser ) {
+
+			$pdo = $this->getPDO();
+
+			return $pdo->read( "SELECT *
+								FROM(
+										SELECT 
+											p.`id`,
+											p.`id_pai`,
+											p.`link`,
+											p.`permissao`,
+											(
+												IF( ISNULL(up.id), 0,1)
+											)ativo
+										FROM permissoes	p
+										LEFT JOIN usuario_permissoes up 
+													ON up.`id_permissao` = p.`id` 
+													AND up.`id_user` = :idUser 
+									) tab", 
+								"idUser={$idUser}");
+			
 		}
 	}
